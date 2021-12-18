f02f6b5ec71dae2e6ede964700c8925fab2c7d52
Islam <alyshbekov96@gmail.com>
2021-12-19 00:50:10 +0600

    array methods

---
+++ starterJS/arrayMethods/main.js
@@ -0,0 +1,6 @@
+let sentence = prompt('Enter your sentence!');
+
+let wordCollection = sentence.split(' ');
+
+console.log('wordCollection.length =>>', wordCollection);
+alert(`word quantity: ${wordCollection.length}`);
