503d7d561a8307d413d7f3c0d34d3daa65254d3f
Islam <alyshbekov96@gmail.com>
2021-12-18 18:35:39 +0600

    base calc js

---
+++ baseJS/calcJS/main.js
@@ -0,0 +1,7 @@
+const number1 = +prompt('enter number 1');
+const number2 = +prompt('enter number 2');
+
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+console.log(`${number1} - ${number2} = ${number1 - number2}`);
+console.log(`${number1} * ${number2} = ${number1 * number2}`);
+console.log(`${number1} / ${number2} = ${number1 / number2}`);
