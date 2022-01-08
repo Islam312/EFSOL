0aa5bd4f1eb77d65ee3b95de53049f83145be274
Islam <alyshbekov96@gmail.com>
2022-01-08 17:41:40 +0600

    download webpack-dev-server

--- ES6/webpack_learning/webpack.config.js
+++ ES6/webpack_learning/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -17,9 +18,20 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  devServer: {
+    port: 4200,
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
     }),
     new CleanWebpackPlugin(),
   ],
